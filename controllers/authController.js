b1838c9331824f7eedbdcc038e5b7978d87542df
Habeba Ahmed Elbaghdady <habebaahmed101@gmail.com>
2026-05-09 12:21:11 +0300

    Minor update

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,8 @@ export async function signup(req, res, next) {
   try {
     const { username, email, password, displayName } = req.body;
     const userExist = await User.findOne({ $or: [{ email }, { username }] });
-    if (userExist) return res(409).json("Email or username already taken");
+    if (userExist)
+      return res.status(409).json("Email or username already taken");
     const hashed = await User.hashPassword(password);
     const user = await User.create({
       username,
